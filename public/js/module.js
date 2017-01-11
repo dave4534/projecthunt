fd1391b6bfb22e52c3d2594003f610c31cd2f84f
Dave <uDave@Main.local>
2017-01-11 23:35:45 +0200

    uirouter attempt

--- public/js/module.js
+++ public/js/module.js
@@ -1,2 +1,30 @@
-var app = angular.module('projectHunt', []);
+var app = angular.module('projectHunt', ['ui.router']);
+
+app.config(['$stateProvider','$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+
+$stateProvider
+    .state('home', {
+    url: '/home',
+    controller: 'mainController',
+    templateUrl: '../home.html'
+    })
+    .state('home.dashboard', {
+    url: '/spotify',
+    controller: 'mainController',
+    templateUrl:'./Templates/dashboard.html'
+    })
+    .state('home.register', {
+    url: '/register',
+    controller: 'mainController',
+    templateUrl:'./Templates/register.html'
+    })
+     .state('home.login', {
+    url: '/login',
+    controller: 'mainController',
+    templateUrl:'./Templates/login.html'
+    });
+
+    $urlRouterProvider.otherwise('/dashboard');
+
+}]);
 
