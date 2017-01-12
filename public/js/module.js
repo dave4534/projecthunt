11b9c632dbedd6935e36e7815efbb4a6ccd6d785
Allon Segall <Segal500@gmail.com>
2017-01-12 11:30:36 +0200

    conflct fix?

--- public/js/module.js
+++ public/js/module.js
@@ -1,1 +1,29 @@
-var app = angular.module('projectHunt', []);
+var app = angular.module('projectHunt', ['ui.router']);
+
+app.config(['$stateProvider','$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+
+$stateProvider
+    .state('home', {
+    url: '/home',
+    controller: 'mainController',
+    templateUrl: './Templates/home.html'
+    })
+    .state('dashboard', {
+    url: '/dashboard',
+    controller: 'projControl',
+    templateUrl:'./Templates/dashboard.html'
+    })
+    .state('register', {
+    url: '/register',
+    controller: 'mainController',
+    templateUrl:'./Templates/register.html'
+    })
+     .state('login', {
+    url: '/login',
+    controller: 'mainController',
+    templateUrl:'./Templates/login.html'
+    });
+
+    $urlRouterProvider.otherwise('/home');
+
+}]);
