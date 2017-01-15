88adf220727a6b115242734879a8b51a12701d36
Dave <uDave@Main.local>
2017-01-15 16:50:21 +0200

    tried to add tag search, still not working

--- public/js/controllers/tagCtrl.js
+++ public/js/controllers/tagCtrl.js
@@ -1,10 +1,9 @@
 app.controller('tagController', ['$scope', 'mainService', function($scope, mainService){
 
 
-  $scope.tags = [
-    { text: 'Tag1' },
-    { text: 'Tag2' },
-    { text: 'Tag3' }
+  $scope.langTags = [
+    { text: 'Angular' },
+    { text: 'JavaScript' }
   ];
 
 
