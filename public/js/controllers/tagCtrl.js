adf8858163d84d04a25a854203a7ec991c70e819
Dave <uDave@Main.local>
2017-01-15 14:03:52 +0200

    tried to implement tags, not working yet, going to sandbox a trial in seperate file

---
+++ public/js/controllers/tagCtrl.js
@@ -0,0 +1,12 @@
+app.controller('tagController', ['$scope', 'mainService', function($scope, mainService){
+
+
+  $scope.tags = [
+    { text: 'Tag1' },
+    { text: 'Tag2' },
+    { text: 'Tag3' }
+  ];
+
+
+
+}]);
