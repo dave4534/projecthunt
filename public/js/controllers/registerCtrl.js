b3d5eba89f48dd2568d219c327dc6b93ecf8c37f
Allon Segall <Segal500@gmail.com>
2017-01-18 21:17:21 +0200

    rgister almost work

--- public/js/controllers/registerCtrl.js
+++ public/js/controllers/registerCtrl.js
@@ -1,6 +1,17 @@
 app.controller('registerController', ['$scope', '$state', function($scope, $state){
 
+  //  = inputId;
+
 // comp = $scope.compName
+$scope.checkName = function(){
+  if ($scope.compName === undefined) {
+    alert("Company must have a name!");
+    $state.go('http://localhost:4008/#/register');
+  }
+  else {
+    $scope.registerSave($scope.compName);
+  }
+}
 
   $scope.registerSave = function(name){
     console.log("button work");
